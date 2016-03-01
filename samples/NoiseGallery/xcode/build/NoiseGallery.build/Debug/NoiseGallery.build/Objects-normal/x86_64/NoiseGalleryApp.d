17fb5565b7981c86075778bd7555d6ede9116c08
simongeilfus <simongeilfus@gmail.com>
2016-03-01 15:35:38 +0100

    Removed sample build/

--- samples/NoiseGallery/xcode/build/NoiseGallery.build/Debug/NoiseGallery.build/Objects-normal/x86_64/NoiseGalleryApp.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Frameworks/Cinder/cinder_fork/blocks/SimplexNoise/samples/NoiseGallery/src/NoiseGalleryApp.cpp \
-  ../../../../../include/cinder/app/RendererGl.h \
-  /Frameworks/Cinder/cinder_fork/blocks/SimplexNoise/include/Simplex.h
--- samples/NoiseGallery/xcode/build/NoiseGallery.build/Debug/NoiseGallery.build/Objects-normal/x86_64/Simplex.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Frameworks/Cinder/cinder_fork/blocks/SimplexNoise/src/Simplex.cpp \
-  /Frameworks/Cinder/cinder_fork/blocks/SimplexNoise/include/Simplex.h
