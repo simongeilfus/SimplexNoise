afe230507afc81f4c7d5799d8c743ea48bebfe30
Simon Geilfus <simongeilfus@users.noreply.github.com>
2017-01-01 13:52:01 +0100

    Made function inline to avoid duplicate definitions

--- include/Simplex.h
+++ include/Simplex.h
@@ -45,117 +45,117 @@
 namespace Simplex {
 
 //! Returns a 1D simplex noise
-float noise( float x );
+inline float noise( float x );
 //! Returns a 2D simplex noise
-float noise( const glm::vec2 &v );
+inline float noise( const glm::vec2 &v );
 //! Returns a 3D simplex noise
-float noise( const glm::vec3 &v );
+inline float noise( const glm::vec3 &v );
 //! Returns a 4D simplex noise
-float noise( const glm::vec4 &v );
+inline float noise( const glm::vec4 &v );
 
 //! Returns a 1D simplex ridged noise
-float ridgedNoise( float x );
+inline float ridgedNoise( float x );
 //! Returns a 2D simplex ridged noise
-float ridgedNoise( const glm::vec2 &v );
+inline float ridgedNoise( const glm::vec2 &v );
 //! Returns a 3D simplex ridged noise
-float ridgedNoise( const glm::vec3 &v );
+inline float ridgedNoise( const glm::vec3 &v );
 //! Returns a 4D simplex ridged noise
-float ridgedNoise( const glm::vec4 &v );
+inline float ridgedNoise( const glm::vec4 &v );
 
 //! Returns a 1D simplex noise with analytical derivative.
-glm::vec2 dnoise( float x );
+inline glm::vec2 dnoise( float x );
 //! Returns a 2D simplex noise with analytical derivatives.
-glm::vec3 dnoise( const glm::vec2 &v );
+inline glm::vec3 dnoise( const glm::vec2 &v );
 //! Returns a 3D simplex noise with analytical derivatives.
-glm::vec4 dnoise( const glm::vec3 &v );
+inline glm::vec4 dnoise( const glm::vec3 &v );
 // not optimal but easiest way to return 5 floats
 typedef std::array<float,5> vec5;
 //! Returns a 4D simplex noise with analytical derivatives
-vec5	dnoise( const glm::vec4 &v );
+inline vec5	dnoise( const glm::vec4 &v );
 	
 //! Returns a 2D simplex cellular/worley noise
-float worleyNoise( const glm::vec2 &v );
+inline float worleyNoise( const glm::vec2 &v );
 //! Returns a 3D simplex cellular/worley noise
-float worleyNoise( const glm::vec3 &v );
+inline float worleyNoise( const glm::vec3 &v );
 //! Returns a 2D simplex smooth cellular/worley noise
-float worleyNoise( const glm::vec2 &v, float falloff );
+inline float worleyNoise( const glm::vec2 &v, float falloff );
 //! Returns a 3D simplex smooth cellular/worley noise
-float worleyNoise( const glm::vec3 &v, float falloff );
+inline float worleyNoise( const glm::vec3 &v, float falloff );
 
 //! Returns a 2D simplex noise with rotating gradients
-float flowNoise( const glm::vec2 &v, float angle );
+inline float flowNoise( const glm::vec2 &v, float angle );
 //! Returns a 3D simplex noise with rotating gradients
-float flowNoise( const glm::vec3 &v, float angle );
+inline float flowNoise( const glm::vec3 &v, float angle );
 
 //! Returns a 2D simplex noise with rotating gradients and analytical derivatives
-glm::vec3 dFlowNoise( const glm::vec2 &v, float angle );
+inline glm::vec3 dFlowNoise( const glm::vec2 &v, float angle );
 //! Returns a 3D simplex noise with rotating gradients and analytical derivatives
-glm::vec4 dFlowNoise( const glm::vec3 &v, float angle );
+inline glm::vec4 dFlowNoise( const glm::vec3 &v, float angle );
 
 //! Returns the curl of a 2D simplex noise
-glm::vec2 curlNoise( const glm::vec2 &v );
+inline glm::vec2 curlNoise( const glm::vec2 &v );
 //! Returns the curl of a 2D simplex flow noise
-glm::vec2 curlNoise( const glm::vec2 &v, float t );
+inline glm::vec2 curlNoise( const glm::vec2 &v, float t );
 //! Returns the curl of a 2D simplex noise fractal brownian motion sum
-glm::vec2 curlNoise( const glm::vec2 &v, uint8_t octaves, float lacunarity, float gain );
+inline glm::vec2 curlNoise( const glm::vec2 &v, uint8_t octaves, float lacunarity, float gain );
 //! Returns the curl of a 3D simplex noise
-glm::vec3 curlNoise( const glm::vec3 &v );
+inline glm::vec3 curlNoise( const glm::vec3 &v );
 //! Returns the curl of a 3D simplex flow noise
-glm::vec3 curlNoise( const glm::vec3 &v, float t );
+inline glm::vec3 curlNoise( const glm::vec3 &v, float t );
 //! Returns the curl approximation of a 3D simplex noise fractal brownian motion sum
-glm::vec3 curlNoise( const glm::vec3 &v, uint8_t octaves, float lacunarity, float gain );
+inline glm::vec3 curlNoise( const glm::vec3 &v, uint8_t octaves, float lacunarity, float gain );
 
 //! Returns the curl of a custom 2D potential using finite difference approximation
-glm::vec2 curl( const glm::vec2 &v, const std::function<float(const glm::vec2&)> &potential, float delta = 1e-4f );
+inline glm::vec2 curl( const glm::vec2 &v, const std::function<float(const glm::vec2&)> &potential, float delta = 1e-4f );
 //! Returns the curl of a custom 3D potential using finite difference approximation
-glm::vec3 curl( const glm::vec3 &v, const std::function<glm::vec3(const glm::vec3&)> &potential, float delta = 1e-4f );
+inline glm::vec3 curl( const glm::vec3 &v, const std::function<glm::vec3(const glm::vec3&)> &potential, float delta = 1e-4f );
 
 //! Returns a 1D simplex noise fractal brownian motion sum
-float fBm( float x, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline float fBm( float x, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 //! Returns a 2D simplex noise fractal brownian motion sum
-float fBm( const glm::vec2 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline float fBm( const glm::vec2 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 //! Returns a 3D simplex noise fractal brownian motion sum
-float fBm( const glm::vec3 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline float fBm( const glm::vec3 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 //! Returns a 4D simplex noise fractal brownian motion sum
-float fBm( const glm::vec4 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline float fBm( const glm::vec4 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 	
 //! Returns a 2D simplex cellular/worley noise fractal brownian motion sum
-float worleyfBm( const glm::vec2 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline float worleyfBm( const glm::vec2 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 //! Returns a 3D simplex cellular/worley noise fractal brownian motion sum
-float worleyfBm( const glm::vec3 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline float worleyfBm( const glm::vec3 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 //! Returns a 2D simplex smooth cellular/worley noise fractal brownian motion sum
-float worleyfBm( const glm::vec2 &v, float falloff, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline float worleyfBm( const glm::vec2 &v, float falloff, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 //! Returns a 3D simplex smooth cellular/worley noise fractal brownian motion sum
-float worleyfBm( const glm::vec3 &v, float falloff, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline float worleyfBm( const glm::vec3 &v, float falloff, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 
 //! Returns a 1D simplex noise fractal brownian motion sum with analytical derivatives
-glm::vec2 dfBm( float x, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline glm::vec2 dfBm( float x, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 //! Returns a 2D simplex noise fractal brownian motion sum with analytical derivatives
-glm::vec3 dfBm( const glm::vec2 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline glm::vec3 dfBm( const glm::vec2 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 //! Returns a 3D simplex noise fractal brownian motion sum with analytical derivatives
-glm::vec4 dfBm( const glm::vec3 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline glm::vec4 dfBm( const glm::vec3 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 //! Returns a 4D simplex noise fractal brownian motion sum with analytical derivatives
-vec5	dfBm( const glm::vec4 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline vec5	dfBm( const glm::vec4 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 	
 //! Returns a 1D simplex ridged multi-fractal noise sum
-float ridgedMF( float x, float ridgeOffset = 1.0f, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline float ridgedMF( float x, float ridgeOffset = 1.0f, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 //! Returns a 2D simplex ridged multi-fractal noise sum
-float ridgedMF( const glm::vec2 &v, float ridgeOffset = 1.0f, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline float ridgedMF( const glm::vec2 &v, float ridgeOffset = 1.0f, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 //! Returns a 3D simplex ridged multi-fractal noise sum
-float ridgedMF( const glm::vec3 &v, float ridgeOffset = 1.0f, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline float ridgedMF( const glm::vec3 &v, float ridgeOffset = 1.0f, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 //! Returns a 4D simplex ridged multi-fractal noise sum
-float ridgedMF( const glm::vec4 &v, float ridgeOffset = 1.0f, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline float ridgedMF( const glm::vec4 &v, float ridgeOffset = 1.0f, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 
 //! Returns the 2D simplex noise fractal brownian motion sum variation by Iñigo Quilez
-float iqfBm( const glm::vec2 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline float iqfBm( const glm::vec2 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 //! Returns the 2D simplex noise fractal brownian motion sum variation by Iñigo Quilez
-float iqfBm( const glm::vec3 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
+inline float iqfBm( const glm::vec3 &v, uint8_t octaves = 4, float lacunarity = 2.0f, float gain = 0.5f );
 
 //! Returns the 2D simplex noise fractal brownian motion sum variation by Iñigo Quilez that use a mat2 to transform each octave
-float iqMatfBm( const glm::vec2 &v, uint8_t octaves = 4, const glm::mat2 &mat = glm::mat2( 1.6, -1.2, 1.2, 1.6 ), float gain = 0.5f );
+inline float iqMatfBm( const glm::vec2 &v, uint8_t octaves = 4, const glm::mat2 &mat = glm::mat2( 1.6, -1.2, 1.2, 1.6 ), float gain = 0.5f );
 
 //! Seeds the permutation table with new random values
-void seed( uint32_t s );
+inline void seed( uint32_t s );
 	
 // implementation
 	
@@ -306,28 +306,28 @@ namespace details {
 	 * float SLnoise = (SimplexNoise1234::noise(x,y,z) + 1.0) * 0.5;
 	 */
 	
-	float  grad( int hash, float x ) {
+	inline float  grad( int hash, float x ) {
 		int h = hash & 15;
 		float grad = 1.0f + (h & 7);   // Gradient value 1.0, 2.0, ..., 8.0
 		if (h&8) grad = -grad;         // Set a random sign for the gradient
 		return ( grad * x );           // Multiply the gradient with the distance
 	}
 	
-	float  grad( int hash, float x, float y ) {
+	inline float  grad( int hash, float x, float y ) {
 		int h = hash & 7;      // Convert low 3 bits of hash code
 		float u = h<4 ? x : y;  // into 8 simple gradient directions,
 		float v = h<4 ? y : x;  // and compute the dot product with (x,y).
 		return ((h&1)? -u : u) + ((h&2)? -2.0f*v : 2.0f*v);
 	}
 	
-	float  grad( int hash, float x, float y , float z ) {
+	inline float  grad( int hash, float x, float y , float z ) {
 		int h = hash & 15;     // Convert low 4 bits of hash code into 12 simple
 		float u = h<8 ? x : y; // gradient directions, and compute dot product.
 		float v = h<4 ? y : h==12||h==14 ? x : z; // Fix repeats at h = 12 to 15
 		return ((h&1)? -u : u) + ((h&2)? -v : v);
 	}
 	
-	float  grad( int hash, float x, float y, float z, float t ) {
+	inline float  grad( int hash, float x, float y, float z, float t ) {
 		int h = hash & 31;      // Convert low 5 bits of hash code into 32 simple
 		float u = h<24 ? x : y; // gradient directions, and compute dot product.
 		float v = h<16 ? y : z;
@@ -339,20 +339,20 @@ namespace details {
 	 * Helper functions to compute gradients in 1D to 4D
 	 * and gradients-dot-residualvectors in 2D to 4D.
 	 */
-	void grad1( int hash, float *gx ) {
+	inline void grad1( int hash, float *gx ) {
 		int h = hash & 15;
 		*gx = 1.0f + (h & 7);   // Gradient value is one of 1.0, 2.0, ..., 8.0
 		if (h&8) *gx = - *gx;   // Make half of the gradients negative
 	}
 	
-	void grad2( int hash, float *gx, float *gy ) {
+	inline void grad2( int hash, float *gx, float *gy ) {
 		int h = hash & 7;
 		*gx = grad2lut[h][0];
 		*gy = grad2lut[h][1];
 		return;
 	}
 	
-	void grad3( int hash, float *gx, float *gy, float *gz ) {
+	inline void grad3( int hash, float *gx, float *gy, float *gz ) {
 		int h = hash & 15;
 		*gx = grad3lut[h][0];
 		*gy = grad3lut[h][1];
@@ -360,7 +360,7 @@ namespace details {
 		return;
 	}
 	
-	void grad4( int hash, float *gx, float *gy, float *gz, float *gw) {
+	inline void grad4( int hash, float *gx, float *gy, float *gz, float *gw) {
 		int h = hash & 31;
 		*gx = grad4lut[h][0];
 		*gy = grad4lut[h][1];
@@ -375,7 +375,7 @@ namespace details {
 	 * gradients-dot-residualvectors in 2D and 3D.
 	 */
 	
-	void gradrot2( int hash, float sin_t, float cos_t, float *gx, float *gy ) {
+	inline void gradrot2( int hash, float sin_t, float cos_t, float *gx, float *gy ) {
 		int h = hash & 7;
 		float gx0 = grad2lut[h][0];
 		float gy0 = grad2lut[h][1];
@@ -384,7 +384,7 @@ namespace details {
 		return;
 	}
 	
-	void gradrot3( int hash, float sin_t, float cos_t, float *gx, float *gy, float *gz ) {
+	inline void gradrot3( int hash, float sin_t, float cos_t, float *gx, float *gy, float *gz ) {
 		int h = hash & 15;
 		float gux = grad3u[h][0];
 		float guy = grad3u[h][1];
